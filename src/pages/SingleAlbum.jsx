ca009c72d5340a01bf9353bc3aee62d4bf95682c
lsteup <loras12@gmail.com>
2024-05-20 11:06:33 +0200

    style single album page

--- src/pages/SingleAlbum.jsx
+++ src/pages/SingleAlbum.jsx
@@ -38,30 +38,38 @@ const SingleAlbum = (props) => {
     const img = album.images[0].url;
     const artist = album.artists[0].name;
     const tracks = album.tracks.items;
-    const tracksNum = album.total_tracks;
 
     return (
-      <article className="p-10">
-        <img src={img} alt="" />
-        <section>
-          <p>{name}</p>
-          <p>{artist}</p>
-          <p>{tracksNum}</p>
+      <article className="p-10 flex gap-10 flex-wrap justify-center content-center">
+        <img className="border-2 border-gray-900 max-h-80" src={img} alt="" />
+        <section className="border-2 border-gray-900 max-w-[600px] min-w-[340px] grow p-3">
+          <div className="flex place-content-between">
+            <div>
+              <p className="font-semibold text-xl ">{name}</p>
+              <p className="text-lg text-gray-500">{artist}</p>
+            </div>
+            <p>price</p>
+          </div>
+          <div className="mt-3">
+            <div className="divide-y">
+              <div></div>
+              {tracks.map((track) => {
+                console.log(track);
+                return (
+                  <div className="flex gap-5" key={track.track_number}>
+                    <p className="text-gray-500">{track.track_number}</p>
+                    <div>{track.name}</div>
+                  </div>
+                );
+              })}
+              <div></div>
+            </div>
+          </div>
+          <div className="flex place-content-between mt-3">
+            <p>quantity</p>
+            <p>add to cart</p>
+          </div>
         </section>
-        <section>
-          {tracks.map((track) => {
-            console.log(track);
-            return (
-              <div key={track.track_number}>
-                <p>{track.name}</p>
-                <p>{track.track_number}</p>
-              </div>
-            );
-          })}
-        </section>
-        <p>price</p>
-        <p>quantity</p>
-        <p>add to cart</p>
       </article>
     );
   }
