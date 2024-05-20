1cd75752337289bc037fa1e33a042da82a77a920
lsteup <loras12@gmail.com>
2024-05-20 08:15:17 +0200

    set up environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 function App() {
   return <Navbar />;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
+import axios from "axios";
+import env from "react-native-dotenv";
 
 const Home = () => {
+  const apiKey = import.meta.env.VITE_SPOTIFY_ACCESS_TOKEN;
+  console.log(apiKey);
+  const fetchAlbums = async () => {
+    const config = {
+      method: "get",
+      url: "https://api.spotify.com/v1/browse/new-releases",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    };
+
+    try {
+      console.log("sending request");
+      const response = await axios(config);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error making request:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("use effect triggered");
+    fetchAlbums();
+  }, []);
   return (
     <>
       <Navbar />
