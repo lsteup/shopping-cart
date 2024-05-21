77db5a28578e8207d98bf52e2bed214722ad4ad2
lsteup <loras12@gmail.com>
2024-05-21 12:27:25 +0200

    finish styling CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
-import record from "/Users/louisasteup/Desktop/shopping-cart/src/images/record.jpg";
 
-const CTA = ({ slogan, search, link }) => {
+const CTA = ({ slogan, search, link, image }) => {
   return (
-    <div className="bg-gradient-to-l from-orange-100 to-sky-100 flex flex-col justify-between border-2 rounded border-zinc-950 mt-4 p-10 items-center bg-orange-200">
-      <div>
-        <p className="text-4xl">{slogan}</p>
+    <div className="bg-gradient-to-l from-orange-100 to-sky-100 flex  justify-between border-2 font-semibold rounded border-zinc-950 mt-4 p-10 items-center gap-6 grow text-center ">
+      <div className="flex flex-col justify-between  font-semibold  items-center gap-6 grow text-center">
+        <div>
+          <p className="text-4xl">{slogan}</p>
+        </div>
+        <Link
+          className="bg-orange-400 p-3 border-2 border-zinc-950 rounded-lg font-semibold hover:bg-orange-500 "
+          to={link}
+        >
+          Explore {search}
+        </Link>
       </div>
-      <Link
-        className="bg-orange-300 p-3 border-2 border-zinc-950 rounded"
-        to={link}
-      >
-        Explore {search}
-      </Link>
+      <img
+        className="hidden md:block lg:hidden xl:block max-w-72 border-2 border-zinc-950"
+        src={image}
+        alt=""
+      />
     </div>
   );
 };
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,20 +1,24 @@
 import CTA from "../components/CTA";
 import Hero from "../components/Hero";
+import record from "/Users/louisasteup/Desktop/shopping-cart/src/images/record.jpg";
+import singer from "/Users/louisasteup/Desktop/shopping-cart/src/images/singer.jpg";
 
 const LandingPage = () => {
   return (
-    <div className="p-5 bg-orange-50">
+    <div className="p-5 bg-orange-50 lg:flex-wrap">
       <Hero />
-      <div className="flex flex-wrap">
+      <div className="flex flex-row gap-4">
         <CTA
           slogan="Stay Ahead of the Beat"
           search="New Releases"
           link="/new"
+          image={record}
         />
         <CTA
           slogan="Connect with Your Favorite Voices"
           search="Artists"
           link="/artists"
+          image={singer}
         />
       </div>
     </div>
